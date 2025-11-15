d934fa796c97155796e72f8242d6398390f2d4ea
Thuan(Tony) Do <dotony380@gmail.com>
2025-11-14 20:06:27 -0700

    week9remove

--- app/week-9/layout.js
+++
@@ -1,9 +0,0 @@
-// Part 4: layout code starts
-
-import { AuthContextProvider } from "./_utils/auth-context";
-
-export default function Layout({ children }) {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
-}
-
-// Part 4: layout code ends
